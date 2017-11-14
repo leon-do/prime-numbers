2ad3ea67fc69c81c34175d5f92678e4863bef504
leondo <leondo123@gmail.com>
2017-11-14 01:07:33 -0700

    mo numbers

--- prime.js
+++ prime.js
@@ -1,3 +1,5 @@
+console.log(bigNumber())
+console.log(bigNumberString())
 console.log(isPrime(17), Math.random())
 
 /* isPrime() only works if primeNumber > 3 */
@@ -11,4 +13,22 @@ function isPrime (primeNumber) {
 
 	/* checks if the naturalNumbers are a non-negative integer */
 	return ( naturalNumber1 % 1 === 0 || naturalNumber2 % 1 === 0) 
+}
+
+
+/* 
+returns a large number 
+Number.MAX_SAFE_INTEGER === 9007199254740991
+*/
+function bigNumber (maxNum = Number.MAX_SAFE_INTEGER){
+	return Math.floor(Math.random() * maxNum)
+}
+
+/* returns a large string */
+function bigNumberString (bits = 2048){
+	let string = ''
+	for (let i = 0; i < bits; i++){
+		string = string + Math.floor(Math.random() * 10)
+	}
+	return string
 }
