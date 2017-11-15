dd4e1b4286d5f6559cd8438a8828a44394da5005
leondo <leondo123@gmail.com>
2017-11-14 21:38:16 -0700

    big primes

--- prime.js
+++ prime.js
@@ -1,10 +1,24 @@
-console.log(bigNumber())
-console.log(bigNumberString())
-console.log(isPrime(17), Math.random())
+const BigNumber = require('big-number');
 
-/* isPrime() only works if primeNumber > 3 */
+console.log(isBigPrime('32416190071'))
+
+/* checks big numbers */
+function isBigPrime (primeNumber) {
+	/**
+	primeNumber: number you want to check if it is prime
+	naturalNumber: should be a non-negative integers
+	 */
+	const naturalNumber1 = BigNumber(primeNumber).subtract(1).div(6)
+	const naturalNumber2 = BigNumber(primeNumber).add(1).div(6)
+
+	/* checks if the naturalNumbers are a non-negative integer */
+	return ( naturalNumber1.rest.number[0] == 0 || naturalNumber2.rest.number[0] == 0) 
+}
+
+/* isPrime() only works if primeNumber > 3 && primeNumber < 9007199254740991 */
 function isPrime (primeNumber) {
 	/**
+	Euler's: 6n + 1 || 6n - 1
 	primeNumber: number you want to check if it is prime
 	naturalNumber: should be a non-negative integers
 	 */
@@ -15,17 +29,8 @@ function isPrime (primeNumber) {
 	return ( naturalNumber1 % 1 === 0 || naturalNumber2 % 1 === 0) 
 }
 
-
-/* 
-returns a large number 
-Number.MAX_SAFE_INTEGER === 9007199254740991
-*/
-function bigNumber (maxNum = Number.MAX_SAFE_INTEGER){
-	return Math.floor(Math.random() * maxNum)
-}
-
 /* returns a large string */
-function bigNumberString (bits = 2048){
+function number (bits = 2048){
 	let string = ''
 	for (let i = 0; i < bits; i++){
 		string = string + Math.floor(Math.random() * 10)
